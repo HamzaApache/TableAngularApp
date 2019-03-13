feafe88fd8b3e3a60d3555436b7a6e000485ef13
Hamza-Yazid Bachir <hamza-yazid.bachir@gfi.fr>
2019-03-13 12:21:03 +0100

    list des candidat

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,24 +3,33 @@ import {Component} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface CandidatList {
+  cdcivil: string;
+  nom: string;
+  email: string;
+  numecan: string;
+  prenom: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: CandidatList[] = [
+ {cdcivil: "M.", nom: "DUMONCEL", email: "test1@test.test", numecan: "41", prenom: "JEAN-CLAUDE"},
+ {cdcivil: "M.", nom: "LACOSTE", email: "test1@test.test", numecan: "62", prenom: "FRANCIS"},
+ {cdcivil: "M.", nom: "GUERASSIMOV", email: "test1@test.test", numecan: "88", prenom: "BORIS"},
+ {cdcivil: "M.", nom: "DESPOIX", email: "test1@test.test", numecan: "129", prenom: "PHILIPPE"},
+ {cdcivil: "M.", nom: "PERELMAN", email: "test1@test.test", numecan: "156", prenom: "MARC"},
+ {cdcivil: "M.", nom: "MANZANO", email: "test1@test.test", numecan: "169", prenom: "FRANCIS"},
+ {cdcivil: "M.", nom: "LAMBERT", email: "test1@test.test", numecan: "184", prenom: "JEAN"},
+ {cdcivil: "M.", nom: "TRIFFAUX", email: "test1@test.test", numecan: "214", prenom: "JEAN PIERRE"},
+ {cdcivil: "M.", nom: "CASTEL", email: "test1@test.test", numecan: "234", prenom: "PIERRE-HENRI"},
+ {cdcivil: "M.", nom: "DEOTTE", email: "test1@test.test", numecan: "364", prenom: "JEAN-LOUIS"},
+ {cdcivil: "M.", nom: "DURET", email: "test1@test.test", numecan: "380", prenom: "SERGE"},
+ {cdcivil: "Mme", nom: "VOISIN", email: "test1@test.test", numecan: "403", prenom: "PATRICIA"},
+ {cdcivil: "M.", nom: "PLAIN", email: "test1@test.test", numecan: "411", prenom: "ANDRE"},
+ {cdcivil: "Mme", nom: "DEBAT", email: "test1@test.test", numecan: "491", prenom: "MICHELLE"},
+ {cdcivil: "M.", nom: "MAUREL", email: "test1@test.test", numecan: "552", prenom: "CHRISTIAN"},
+ {cdcivil: "M.", nom: "NOSMAS", email: "test1@test.test", numecan: "592", prenom: "JEAN CLARENCE"},
+ {cdcivil: "Mme", nom: "LANDAIS", email: "test1@test.test", numecan: "694", prenom: "JANINE"},
+ {cdcivil: "M.", nom: "MONTESSE", email: "test1@test.test", numecan: "735", prenom: "ALAIN"}
 ];
 
 /**
@@ -32,7 +41,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'table.component.html',
 })
 export class TableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['cdcivil', 'nom','prenom', 'numecan', 'email'];
   dataSource = ELEMENT_DATA;
 }
 
